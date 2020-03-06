985c3d924d0c3b4916bbcfa869f23e4cc077e28b
devcao <caoxiangyang93@gmail.com>
2020-03-06 14:19:48 -0500

    Update depenDesign use fast random generator rmvn

--- R/GenerateData.R
+++ R/GenerateData.R
@@ -28,50 +28,33 @@ dataGen = function(setting = 'dep', ...){
 #' 				if string, c("equl", "weak_equl")
 #' @return a list with X and Y
 #' @export
-depenDesign = function(n, p, beta, rho){
-
-	if(rho == 'equl'){  # equi corr
-    	Sigma = matrix(rep(0.8,p*p),p,p)
-    	diag(Sigma) = rep(1,p)
-
-    	Mu=rep(0,p)
-		X=rmvnorm(n,mean=Mu,sigma=Sigma)
-		Y <- X %*% beta + rnorm(n,0,1)   
-   
-		}else if (rho == "weak_equl"){
-				
-				Sigma = matrix(rep(0.5,p*p),p,p)
-    			diag(Sigma) = rep(1,p)
-
-    			Mu=rep(0,p)
-				X=rmvnorm(n,mean=Mu,sigma=Sigma)
-				Y <- X %*% beta + rnorm(n,0,1)   
-   
-			}else if (rho > 0){  # Topliez corr
-
-    		Sigma = diag(p)
-
-    		for(i in 1:p){
-      			for(j in 1:p){
-        			Sigma[i,j]=rho^(abs(i-j))
-        		}
-    		}
-
-    		Mu = rep(0,p)
-			X = rmvnorm(n,mean=Mu,sigma=Sigma)
-			Y <- X %*% beta + rnorm(n,0,1)  
-
-			}else if (rho == 0){  # independent
-
-				X <- matrix(rnorm(n*p),nrow=n)
-				Y <- X %*% beta + rnorm(n,0,1)
-			
-			}
-
-
-	return(list(X = X, Y = Y))   
-
+depenDesign
+function(n, p, beta, rho){
+  if(rho == 'equl'){  # equi corr
+    Sigma = matrix(rep(0.8,p*p),p,p)
+    diag(Sigma) = rep(1,p)
+    Mu=rep(0,p)
+    X=rmvn(n, mu = Mu,sigma = Sigma)
+    Y <- X %*% beta + rnorm(n,0,1)
+  }else if (rho == "weak_equl"){
+    Sigma = matrix(rep(0.5,p*p),p,p)
+    diag(Sigma) = rep(1,p)
+    Mu=rep(0,p)
+    X=rmvn(n, mu = Mu, sigma = Sigma)
+    Y <- X %*% beta + rnorm(n,0,1)
+  }else if (rho > 0){  # toeplitz matrix
+    Sigma = toeplitz( rho^(0:(p - 1)) )
+    Mu = rep(0,p)
+    #X = rmvnorm(n,mean = Mu,sigma = Sigma)
+    X = rmvn(n, mu = Mu,sigma = Sigma)
+    Y <- X %*% beta + rnorm(n,0,1)
+  }else if (rho == 0){  # independent
+    Sigma = diag(rep(1,p))
+    Mu=rep(0,p)
+    X <- matrix(rnorm(n*p), n, p)
+    Y <- X %*% beta + rnorm(n,0,1)
+  }
+  return(list(X = X, Y = Y))
 }
-
 
 # Setting 2
